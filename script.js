a9f14e5ea8bd1fd0d7135a5408f975431bea0780
Kacper Gajewski <kacper.gajewski@op.pl>
2022-07-13 01:12:45 +0200

    init repo

---
+++ script.js
@@ -0,0 +1,14 @@
+// const inputSearchEl = document.querySelector('.search__input');
+// const searchEl = document.querySelector('.search__button');
+
+// const changeBackground = () => {
+//     if (inputSearchEl.classList.contains(':active') || inputSearchEl.classList.contains(':focus')) {
+//         searchEl.style.background === '#f0eeee';
+//         console.log('udało sie zmienic');
+//         return;
+//     }
+// };
+
+// const searchBox = document.querySelector('.search');
+
+// searchBox.addEventListener('click', changeBackground);
